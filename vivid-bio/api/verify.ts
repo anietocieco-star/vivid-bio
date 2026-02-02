e0bc401b79b398d3ce10762db208ea6d7c933989
anietocieco-star <anietocieco@gmail.com>
2026-02-02 01:24:50 +0100

    Simplify handler function by removing type imports

--- vivid-bio/api/verify.ts
+++ vivid-bio/api/verify.ts
@@ -1,8 +1,3 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default function handler(req, res) {
   res.status(200).json({ ok: true });
 }
